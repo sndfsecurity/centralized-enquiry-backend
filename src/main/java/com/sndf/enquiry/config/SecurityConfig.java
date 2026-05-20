af6b9caf0bb1422177835687b3a4ac305a0e9a22
sndfsecurity <sndf4315@gmail.com>
2026-05-20 16:31:15 +0530

    final security config

--- src/main/java/com/sndf/enquiry/config/SecurityConfig.java
+++ src/main/java/com/sndf/enquiry/config/SecurityConfig.java
@@ -4,17 +4,12 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.Customizer;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.web.cors.CorsConfiguration;
-
 import org.springframework.web.cors.CorsConfigurationSource;
-
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
@@ -24,13 +19,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-
             .cors(Customizer.withDefaults())
-
             .csrf(csrf -> csrf.disable())
 
             .authorizeHttpRequests(auth -> auth
-
                 .requestMatchers(
                     "/api/auth/**",
                     "/api/enquiry",
@@ -49,9 +41,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-            "https://centralized-enquiry-admin.vercel.app",
-            "https://www.detectiveinvestigation.in",
-            "https://detectiveinvestigation.in"
+            "https://centralized-enquiry-admin.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
@@ -67,7 +57,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+            new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", configuration);
 
