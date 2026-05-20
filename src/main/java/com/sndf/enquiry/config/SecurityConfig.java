22e22c04ac77d845595a018e507ef013a86b393f
sndfsecurity <sndf4315@gmail.com>
2026-05-20 15:38:07 +0530

    security updated

--- src/main/java/com/sndf/enquiry/config/SecurityConfig.java
+++ src/main/java/com/sndf/enquiry/config/SecurityConfig.java
@@ -5,25 +5,26 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.web.cors.CorsConfiguration;
+
 import org.springframework.web.cors.CorsConfigurationSource;
+
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+
             .cors(Customizer.withDefaults())
 
             .csrf(csrf -> csrf.disable())
@@ -45,13 +46,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration =
-                new CorsConfiguration();
+        CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
             "https://centralized-enquiry-admin.vercel.app",
             "https://www.detectiveinvestigation.in",
-            "http://localhost:3000"
+            "https://detectiveinvestigation.in"
         ));
 
         configuration.setAllowedMethods(List.of(
@@ -69,10 +69,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration(
-            "/**",
-            configuration
-        );
+        source.registerCorsConfiguration("/**", configuration);
 
         return source;
     }
